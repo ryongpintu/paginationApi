54837da6a48f68cfc9b6a9a0c48de4aff33c34e7
ryongpintu <ryongpintu@gmail.com>
2018-11-06 12:01:28 +0530

    change in cors file

--- MIddleware/resHeader.js
+++ MIddleware/resHeader.js
@@ -3,7 +3,7 @@
 module.exports=function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://ryongpintu.github.io/paginationDemo ,https://ryongpintu.github.io/paginationDemo/,https://ryongpintu.github.io,https://ryongpintu.github.io/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ryongpintu.github.io');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
