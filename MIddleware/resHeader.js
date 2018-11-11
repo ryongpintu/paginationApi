1bee8d0f3e07107b1a4883092f69252756594fa9
ryongpintu <ryongpintu@gmail.com>
2018-11-11 13:31:17 +0530

    header changed

--- MIddleware/resHeader.js
+++ MIddleware/resHeader.js
@@ -3,7 +3,7 @@
 module.exports=function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://ryongpintu.github.io');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
